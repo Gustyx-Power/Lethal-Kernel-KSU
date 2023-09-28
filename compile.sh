7983d1005777557bd04cfd256320f824f0e367bc
kamiaya <arvinnaufal887@gmail.com>
2023-09-28 09:53:52 +0000

    change kernelsu latest to main

--- compile.sh
+++ compile.sh
@@ -8,7 +8,7 @@ yellow='\033[0;33m'
 red='\033[0;31m'
 nocol='\033[0m'
 
-curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
+curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s main
 
 # Cleanup
 rm -rf out/outputs/*
