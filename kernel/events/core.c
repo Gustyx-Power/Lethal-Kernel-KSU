1364f7e17a2a074e382be08e8ad427b209b9adc5
Gustyx-Power Projects <gustiadityamuzaky08@gmail.com>
2024-01-25 00:42:10 +0000

    Remake 520kib To 490kib

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -431,15 +431,15 @@ int sysctl_perf_event_paranoid __read_mostly = 3;
 int sysctl_perf_event_paranoid __read_mostly = 2;
 #endif
 
-/* Minimum for 512 kiB + 1 user control page */
-int sysctl_perf_event_mlock __read_mostly = 512 + (PAGE_SIZE / 1024); /* 'free' kiB per user */
+/* Minimum for 490 kiB + 1 user control page */
+int sysctl_perf_event_mlock __read_mostly = 490 + (PAGE_SIZE / 1024); /* 'free' kiB per user */
 
 /*
  * max perf event sample rate
  */
 #define DEFAULT_MAX_SAMPLE_RATE		100000
 #define DEFAULT_SAMPLE_PERIOD_NS	(NSEC_PER_SEC / DEFAULT_MAX_SAMPLE_RATE)
-#define DEFAULT_CPU_TIME_MAX_PERCENT	12	
+#define DEFAULT_CPU_TIME_MAX_PERCENT	8	
 
 int sysctl_perf_event_sample_rate __read_mostly	= DEFAULT_MAX_SAMPLE_RATE;
 
